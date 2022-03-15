6001abe534f0dc23c0d06c70b06d3571460676fc
Eivind Strand Harboe <eiha0403@gmail.com>
2022-03-15 12:14:36 +0100

    Added: Very simple client

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,14 +1,34 @@
 package client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class Client {
 
-    //TODO: Add logic
 
 
+    //Simple client for now
+    public static void main(String[] args) throws IOException {
+        String ip = "";
+        int port = 80;
+        Socket clientSocket = new Socket(ip, port);
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-    public static void main(String[] args) {
-        //TODO: add all
-        
+        if (clientSocket.isConnected()){
+            System.out.println("Connection Acquired");
+        }
+
+        System.out.println("\n" + in.readLine());
+
+        out.close();
+        in.close();
+        clientSocket.close();
+
+        //TODO: Add more options for communication
     }
 
 
