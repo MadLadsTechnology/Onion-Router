4ed56a6a63134b8c35316b9be422b819487afb11
Anders Heftøy Carlsen <anders.heftoy@gmail.com>
2022-03-15 14:46:52 +0100

    Added: encrypt and decrypt in NodeThread

--- src/main/java/nodeManager/NodeThread.java
+++ src/main/java/nodeManager/NodeThread.java
@@ -1,10 +1,19 @@
 package nodeManager;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
 
 /**
  * Class to handle the node logic.
@@ -47,18 +56,36 @@ public class NodeThread extends Thread {
             //Todo: check if message is encrypted
 
             if(messageIsEncrypted){
-
+                message = rsaDecrypt(message, thisNode.getPrivateKey());
+            } else{
+                message = rsaEncrypt(message.getBytes(), thisNode.getPublicKey());
             }
 
             reader.close();
             writer.close();
             inputStream.close();
-        } catch ( IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
 
     }
+    public String rsaDecrypt(String encryptedMessage, PrivateKey privateKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        byte[] bytes = Base64.getDecoder().decode(encryptedMessage);
 
+        Cipher decryptCipher = Cipher.getInstance("RSA");
+        decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
+
+        return new String(decryptCipher.doFinal(bytes));
+    }
+
+    public static String rsaEncrypt(byte[] plainText, PublicKey publicKey)throws Exception {
+        Cipher encryptCipher = Cipher.getInstance("RSA");
+        encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+        byte[] cipherText = encryptCipher.doFinal(plainText);
+
+        return Base64.getEncoder().encodeToString(cipherText);
+    }
 
 }
