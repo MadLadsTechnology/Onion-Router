4db0252bac7cd560e6f0f1684d890274f6b920ed
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2022-03-16 12:52:21 +0100

    Created: encryptionService

---
+++ src/main/java/crypto/encryptionService.java
@@ -0,0 +1,31 @@
+package crypto;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Base64;
+
+public class encryptionService {
+    public static String rsaDecrypt(String encryptedMessage, PrivateKey privateKey) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        byte[] bytes = Base64.getDecoder().decode(encryptedMessage);
+
+        Cipher decryptCipher = Cipher.getInstance("RSA");
+        decryptCipher.init(Cipher.DECRYPT_MODE, privateKey);
+
+        return new String(decryptCipher.doFinal(bytes));
+    }
+
+    public static String rsaEncrypt(byte[] plainText, PublicKey publicKey)throws Exception {
+        Cipher encryptCipher = Cipher.getInstance("RSA");
+        encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+
+        byte[] cipherText = encryptCipher.doFinal(plainText);
+
+        return Base64.getEncoder().encodeToString(cipherText);
+    }
+}
