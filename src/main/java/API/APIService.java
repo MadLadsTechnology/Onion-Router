33a9132a68404d8479f12c6d9cd4c5080d36aac9
Adrian Wist Hakvåg <adrianwisthakvag@gmail.com>
2022-03-16 16:36:48 +0100

    Added: api post method

--- src/main/java/API/APIService.java
+++ src/main/java/API/APIService.java
@@ -5,11 +5,14 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
+import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 public class APIService {
 
@@ -37,28 +40,23 @@ public class APIService {
 
     }
 
-    public static String apiPOSTRequest(String url) throws Exception {
+    public static void apiPOSTRequest(String url, String publicKey, String address) throws Exception {
 
-        URL urlForGetRequest = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection) urlForGetRequest.openConnection();
-        connection.setRequestMethod("GET");
-        int responseCode = connection.getResponseCode();
+        URL urlPost = new URL(url);
+        HttpURLConnection http = (HttpURLConnection) urlPost.openConnection();
+        http.setRequestMethod("POST");
+        http.setDoOutput(true);
+        http.setRequestProperty("Content-Type", "application/json");
 
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            StringBuilder response = new StringBuilder();
-            String readLine;
-            while ((readLine = in.readLine()) != null) {
-                response.append(readLine);
-            }
-            in.close();
+        String data = "{ 'publicKey:'" + "'" + publicKey +"', 'address':" + "'" + address + "'";
 
-            return String.valueOf(response);
+        byte[] out = data.getBytes(StandardCharsets.UTF_8);
 
-        } else {
-            throw new Exception("Could not connect");
-        }
+        OutputStream stream = http.getOutputStream();
+        stream.write(out);
 
+        System.out.println(http.getResponseCode() + " " + http.getResponseMessage());
+        http.disconnect();
     }
 
 
